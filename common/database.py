7fef5e8b439ab7fc63bd70f2893e075b401e0f84
x4nth055 <37851086+x4nth055@users.noreply.github.com>
2019-04-16 01:15:53 +0200

    added product entity

--- common/database.py
+++ common/database.py
@@ -1,5 +1,6 @@
 import sqlite3
 from common.utils import get_query
+from collections import defaultdict
 
 class Database:
     URL = "db/first_db.sqlite3"
@@ -16,9 +17,19 @@ class Database:
         "TYPE": "VARCHAR"
     }
 
+    PRODUCT_FIELDS = {
+        "ID": "VARCHAR",
+        "NAME": "VARCHAR",
+        "PRICE": "INTEGER",
+        "DESCRIPTION": "TEXT",
+        "IMAGE": "TEXT", # absolute path of the image associated with this product
+        "TAGS": "TEXT", # categories separated by '/' (e.g adventure/thiller/action) 
+    }
+
     # all table queries need to be here
     TABLE_QUERIES = [
-        get_query("USER", USER_FIELDS)
+        get_query("USER", USER_FIELDS),
+        get_query("PRODUCT", PRODUCT_FIELDS)
     ]
         
     @classmethod
@@ -27,6 +38,9 @@ class Database:
         # create tables
         for query in cls.TABLE_QUERIES:
             cls.DATABASE.execute(query)
+
+
+    ### User Entity ###
 
     @classmethod
     def save_user(cls, **kwargs):
@@ -66,4 +80,51 @@ class Database:
     def get_user_by_email(cls, email):
         """Get user data from its email, more info at `get_user_by_id`"""
         return cls._get_user_by("EMAIL", email)
+
+
+    ### Product entity ###
+    @classmethod
+    def save_product(cls, **kwargs):
+        id = kwargs.get("id")
+        name = kwargs.get("name")
+        price = kwargs.get("price")
+        description = kwargs.get("description")
+        image = kwargs.get("image")
+        tags = kwargs.get("tags")
+        parameters = (id, name, price, description, image, tags)
+        cls.DATABASE.execute("INSERT INTO PRODUCT VALUES (?, ?, ?, ?, ?, ?)", parameters)
+        cls.DATABASE.commit()
+
+    @classmethod
+    def _get_product_by(cls, key, value):
+        cursor = cls.DATABASE.execute(f"SELECT * FROM PRODUCT WHERE {key}=?", (value,))
+        returned_data = cursor.fetchone()
+        if returned_data is None:
+            return None
+        data = {}
+        for i, field in enumerate(cls.PRODUCT_FIELDS):
+            data[field.lower()] = returned_data[i]
+        return data
+
+    @classmethod
+    def get_product_by_id(cls, id):
+        return cls._get_product_by("ID", id)
+
+    @classmethod
+    def get_products_by_tag(cls, tag):
+        """Returns all products tagged by `tag`
+        Format is a list of dicts"""
+        cursor = cls.DATABASE.execute("SELECT * FROM PRODUCT WHERE TAGS LIKE ?", (f"%{tag}%",))
+        returned_data = cursor.fetchall()
+        # [(1, 'test1', 'test1'), (2, 'test2', 'test2')]
+        if not returned_data:
+            return None
+        data = []
+        for item in returned_data:
+            d = {}
+            for i, field in enumerate(cls.PRODUCT_FIELDS):
+                d[field] = item[i]
+            data.append(d)
+        return data
+        
     
